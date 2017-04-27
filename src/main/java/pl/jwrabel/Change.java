b80abaff1ed6c70351f60facf9e93be5e97f45de
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-27 18:01:58 +0200

    asdfghjk

---
+++ src/main/java/pl/jwrabel/Change.java
@@ -0,0 +1,13 @@
+package pl.jwrabel;
+
+import java.util.List;
+
+/**
+ * Created by jakubwrabel on 27.04.2017.
+ */
+public class Change {
+    public List<Integer> getChangeNominals(int amount){
+
+        return null;
+    }
+}
